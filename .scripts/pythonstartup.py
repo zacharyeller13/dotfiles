183c4a44846d38ff179535ca99917994821c7b8d
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2023-10-02 21:16:24 -0500

    Add pythonstartup script

---
+++ .scripts/pythonstartup.py
@@ -0,0 +1,29 @@
+""""Startup file for Python REPL"""
+import atexit
+import json
+import os
+import sys
+
+from datetime import datetime, date, timedelta
+from pprint import pprint
+
+try:
+    import requests
+except ImportError:
+    pass
+
+try:
+    import asyncio
+except ImportError:
+    pass
+
+try:
+    # Will only work on Linux; readline is not intended for Windows
+    import readline
+    readline.parse_and_bind("tab: complete")
+    print('letz go.')
+    del readline
+except ImportError:
+    print('no readline support.')
+
+VENV = os.environ.get("VIRTUAL_ENV")
