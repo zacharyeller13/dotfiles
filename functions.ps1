84ec39cf91e2926d9816055f4c2b965baa79f33e
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2024-02-20 11:28:25 -0600

    refactor: update Clear-IntappCache to add some advanced function syntax

--- functions.ps1
+++ functions.ps1
@@ -4,9 +4,16 @@ function gotoNand2Tetris { Set-Location "C:\Users\zachary.eller.WILSONLEGAL\Next
 function gotoProjects { Set-Location "$HOME\OneDrive - Harbor Global\Documents\Projects" }
 function gotoScripts { Set-Location "$HOME\OneDrive - Harbor Global\Documents\Scripts" }
 
-function ClearIntappCache {
-	$dataCache = "$env:APPDATA\Intapp\Time\Data";
-	if (Test-Path $dataCache) {
-		Remove-Item -Recurse -Force $dataCache;
+# To clear the Intapp Data Cache when troubleshooting or other reasons
+function Clear-IntappCache {
+	[CmdletBinding(SupportsShouldProcess=$true)]
+	Param()
+
+	Process
+	{
+		$dataCache = "$env:APPDATA\Intapp\Time\Data";
+		if (Test-Path $dataCache) {
+			Remove-Item -Recurse -Force $dataCache;
+		}
 	}
 }
