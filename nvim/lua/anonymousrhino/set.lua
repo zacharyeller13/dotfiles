b285fad136b03d65141df2dde814a363d6f35906
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2024-02-26 15:22:02 -0600

    feat(nvim): Update set.lua to support Windows

--- nvim/lua/anonymousrhino/set.lua
+++ nvim/lua/anonymousrhino/set.lua
@@ -18,7 +18,7 @@ vim.opt.wrap = false
 -- Long-running undos
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv("HOME") or os.getenv("USERPROFILE") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 -- Stop keeping search terms highlighted
