3d86d45ce81941745c3fab832f883013f845639b
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2024-03-16 02:16:18 -0500

    feat(install-script): Create initial install bash script.
    Also added bash-lsp-server to the Mason config for neovim.

--- kickstartnvim/init.lua
+++ kickstartnvim/init.lua
@@ -550,6 +550,7 @@ require('lazy').setup({
                     },
                 },
                 ruff_lsp = {},
+                bashls = {},
                 -- rust_analyzer = {},
                 -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
                 --
