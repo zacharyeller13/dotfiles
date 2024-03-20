852f56564329dd706b084be70d22a6fbcb7925cf
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2024-03-20 17:31:10 -0500

    feat(nvim-harpoon): Add keymaps for harpoon buffer switching

--- kickstartnvim/init.lua
+++ kickstartnvim/init.lua
@@ -866,6 +866,18 @@ end)
 vim.keymap.set('n', '<C-e>', function()
     harpoon.ui:toggle_quick_menu(harpoon:list())
 end)
+vim.keymap.set('n', '<C-h>', function()
+    harpoon:list():select(1)
+end)
+vim.keymap.set('n', '<C-j>', function()
+    harpoon:list():select(2)
+end)
+vim.keymap.set('n', '<C-k>', function()
+    harpoon:list():select(3)
+end)
+vim.keymap.set('n', '<C-l>', function()
+    harpoon:list():select(4)
+end)
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=4 sts=4 sw=4 et
