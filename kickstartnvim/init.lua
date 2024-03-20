eff7464377b80af9a7536d584d3e97313543e003
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2024-03-20 15:56:52 -0500

    feat(neovim): Remove comments from init.lua

--- kickstartnvim/init.lua
+++ kickstartnvim/init.lua
@@ -57,10 +57,6 @@ Kickstart Guide:
 
 If you experience any errors while trying to install kickstart, run `:checkhealth` for more info
 
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
 -- Set <space> as the leader key
