fb28cf78be57181c4b9c41fbd16cac3e32ded7d9
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2024-07-08 10:06:13 -0500

    refactor(neovim): Update keymaps

--- kickstartnvim/lua/custom/plugins/keymaps.lua
+++ kickstartnvim/lua/custom/plugins/keymaps.lua
@@ -1,6 +1,12 @@
 -- Setup to use netrw
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = 'Open Files' })
+
+-- Format using conform
 vim.keymap.set('n', '<leader>f', function()
     require('conform').format { async = true }
 end, { desc = '[F]ormat file' })
+
+-- Keep the cursor in the middle when paging up/down
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
 return {}
