adda8670e7f59af68e39ecdef38501e4f7fb68d0
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2024-08-09 08:21:12 -0500

    feat(nvim): Add `sqlfmt` to `conform` setup
    Still not working at the moment, need to investigate.  Also added the
    checkhealth suggestions for `which-key`; will implement later

--- kickstartnvim/init.lua
+++ kickstartnvim/init.lua
@@ -278,6 +278,42 @@ require('lazy').setup({
                 ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
                 ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
                 ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+                --   {
+                --   ["<leader>c"] = {
+                --   _ = "which_key_ignore",
+                --   name = "[C]ode"
+                --   },
+                --   ["<leader>d"] = {
+                --   _ = "which_key_ignore",
+                --   name = "[D]ocument"
+                --   },
+                --   ["<leader>r"] = {
+                --   _ = "which_key_ignore",
+                --   name = "[R]ename"
+                --   },
+                --   ["<leader>s"] = {
+                --   _ = "which_key_ignore",
+                --   name = "[S]earch"
+                --   },
+                --   ["<leader>w"] = {
+                --   _ = "which_key_ignore",
+                --   name = "[W]orkspace"
+                --   }
+                --   }
+                --
+                --   -- Suggested Spec:
+                --   {
+                --   { "<leader>c", group = "[C]ode" },
+                --   { "<leader>c_", hidden = true },
+                --   { "<leader>d", group = "[D]ocument" },
+                --   { "<leader>d_", hidden = true },
+                --   { "<leader>r", group = "[R]ename" },
+                --   { "<leader>r_", hidden = true },
+                --   { "<leader>s", group = "[S]earch" },
+                --   { "<leader>s_", hidden = true },
+                --   { "<leader>w", group = "[W]orkspace" },
+                --   { "<leader>w_", hidden = true },
+                --   }
             }
         end,
     },
@@ -644,6 +680,7 @@ require('lazy').setup({
                 -- You can use a sub-list to tell conform to run *until* a formatter
                 -- is found.
                 -- javascript = { { "prettierd", "prettier" } },
+                sql = { 'sqlfluff' },
             },
         },
     },
