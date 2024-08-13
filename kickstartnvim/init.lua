20d674adf5b7c29c8a64d7e806716e9e6154f04c
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2024-08-13 18:15:16 -0500

    refactor(nvim): Remove mypy - not using

--- kickstartnvim/init.lua
+++ kickstartnvim/init.lua
@@ -564,13 +564,15 @@ require('lazy').setup({
                         },
                     },
                 },
-                ruff_lsp = { settings = { show_fixes = true } },
-                -- ruff = {
-                --     init_options = {
-                --         settings = {},
-                --     },
-                -- },
-                mypy = {},
+                -- ruff_lsp = { settings = { show_fixes = true } },
+                ruff = {
+                    trace = 'messages',
+                    init_options = {
+                        settings = {
+                            logLevel = 'debug',
+                        },
+                    },
+                },
                 bashls = {},
                 -- rust_analyzer = {},
                 -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
