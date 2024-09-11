5e1b2660faa063a52a069408e07e2cfeab041cfa
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2024-09-11 17:49:47 -0500

    feat(scripts):  Add alias for script and wezterm config to paste from clipboard

--- wezterm/keybindings.lua
+++ wezterm/keybindings.lua
@@ -1,15 +1,21 @@
 local wezterm = require("wezterm")
 local module = {}
+local act = wezterm.action
 
 function module.bind_keys(config)
 	config.keys = {
 		{
 			key = ",",
 			mods = "SUPER",
-			action = wezterm.action.SpawnCommandInNewTab({
+			action = act.SpawnCommandInNewTab({
 				cwd = wezterm.home_dir,
 				args = { "nvim", wezterm.home_dir .. "/.config/wezterm/wezterm.lua" },
 			}),
+		},
+		{
+			key = "V",
+			mods = "CTRL",
+			action = act.PasteFrom("Clipboard"),
 		},
 	}
 end
