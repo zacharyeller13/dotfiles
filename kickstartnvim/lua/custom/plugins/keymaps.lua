645e847200d4963e9eecaae090747278017a477a
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2025-01-09 15:55:30 -0600

    refactor(nvim): Update nvim config

--- kickstartnvim/lua/custom/plugins/keymaps.lua
+++ kickstartnvim/lua/custom/plugins/keymaps.lua
@@ -1,6 +1,11 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+-- Run lua code when necessary
+vim.keymap.set('n', '<space><space>x', '<cmd>source %<CR>')
+vim.keymap.set('n', '<space>x', ':.lua<CR>')
+vim.keymap.set('v', '<space>x', ':lua<CR>')
+
 -- Setup to use netrw
 -- vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 -- Setup to use netrw
