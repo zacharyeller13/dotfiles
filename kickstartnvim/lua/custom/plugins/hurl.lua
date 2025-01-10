67df9e0706a7f321efe3690c45d7066a029554e3
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2025-01-10 15:41:38 -0600

    feat(hurl.nvim): Add hurl.dev plugin

--- kickstartnvim/lua/custom/plugins/hurl.lua
+++ kickstartnvim/lua/custom/plugins/hurl.lua
@@ -9,7 +9,7 @@ return {
     opts = {
         debug = false,
         show_notification = false,
-        mode = 'split',
+        mode = 'popup',
         formatters = {
             json = { 'jq' },
             html = {
@@ -18,5 +18,21 @@ return {
                 'html',
             },
         },
+        mappings = {
+            close = 'q',
+            next_panel = '<C-n>',
+            prev_panel = '<C-p>',
+        },
+    },
+    keys = {
+        { '<leader>A', '<cmd>HurlRunner<CR>', desc = 'Run All requests' },
+        { '<leader>a', '<cmd>HurlRunnerAt<CR>', desc = 'Run Api request' },
+        { '<leader>te', '<cmd>HurlRunnerToEntry<CR>', desc = 'Run Api request to entry' },
+        { '<leader>tE', '<cmd>HurlRunnerToEnd<CR>', desc = 'Run Api request from current entry to end' },
+        { '<leader>tm', '<cmd>HurlToggleMode<CR>', desc = 'Hurl Toggle Mode' },
+        { '<leader>tv', '<cmd>HurlVerbose<CR>', desc = 'Run Api in verbose mode' },
+        { '<leader>tV', '<cmd>HurlVeryVerbose<CR>', desc = 'Run Api in very verbose mode' },
+        -- Run Hurl request in visual mode
+        { '<leader>h', ':HurlRunner<CR>', desc = 'Hurl Runner', mode = 'v' },
     },
 }
