cef98d72de4e7dbbdcd96c57f8e7d03caecd1891
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2025-02-15 19:01:44 -0600

    chore:  Clean dev.lua file

--- nvim/lua/custom/dev.lua
+++ nvim/lua/custom/dev.lua
@@ -1,26 +1,3 @@
 local M = {}
 
----@type vim.SystemObj
-local job
-
-M.start = function()
-    ---@param out vim.SystemCompleted
-    local on_exit = function(out)
-        vim.print(out.code)
-        vim.print(out.signal)
-    end
-    job = vim.system({ 'ollama', 'serve' }, {}, on_exit)
-end
-
-vim.api.nvim_create_autocmd('VimLeavePre', {
-    group = vim.api.nvim_create_augroup('OllamaCmds', { clear = true }),
-    callback = function(ev)
-        if job then
-            job:kill(9)
-            vim.print(job)
-        end
-    end,
-    desc = 'Kills `ollama serve` command before exiting vim',
-})
-
 return M
