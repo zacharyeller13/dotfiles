6f713b67ba9eb55f3b833e70a591ba660475182e
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2025-09-29 12:59:10 -0500

    feat(hammerspoon): Add full hammerspoon configurations including icon and spoons

---
+++ macos/hammerspoon/README.md
@@ -0,0 +1,3 @@
+My Personalized Hammerspoon Setup. Pretty basic.
+
+The EmmyLua spoon has a few customized annotations b/c some of the annotations don't seem to be set by the current spoon version and it's easier for me to just update an individual annotation when I use it than to try to actually update the entire spoon
--- macos/hammerspoon/init.lua
+++ macos/hammerspoon/init.lua
@@ -22,12 +22,15 @@ hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "a", function()
     show_chooser()
 end)
 
-function test()
-    print(hs.inspect(hs.audiodevice.current()))
-    for _, device in ipairs(hs.audiodevice.allDevices()) do
-        print(device:name(), device:inUse())
-    end
+-- Due to symlinks, there may not be a currentDir set at startup; we want to run
+-- everything as if from the config dir
+if not hs.fs.chdir(hs.configdir) then
+    error("Error changing dir to " .. hs.configdir)
 end
+assert(
+    hs.fs.currentDir() == hs.fs.pathToAbsolute(hs.configdir),
+    string.format("cwd %s must be the same as the configdir %s", hs.fs.currentDir(), hs.fs.pathToAbsolute(hs.configdir))
+)
 
 ---@param to string Audio device name to switch to
 function change_output(to)
@@ -41,17 +44,22 @@ function change_output(to)
     end
 end
 
+---Show audio switching chooser
 function show_chooser()
+    local audio_image = hs.image.imageFromPath("icons/speaker.png")
+
     local chooser = hs.chooser.new(function(choice)
         print("Choice: " .. hs.inspect(choice))
-        change_output(choice.text)
-        print(hs.audiodevice.current().name)
+        if choice then
+            change_output(choice.text)
+            print(hs.audiodevice.current().name)
+        end
     end)
     chooser:choices(function()
-        local devices = hs.audiodevice.allDevices()
+        local devices = hs.audiodevice.allOutputDevices()
         local choices = {}
         for _, device in ipairs(devices) do
-            table.insert(choices, { text = device:name(), subText = device:uid() })
+            table.insert(choices, { image = audio_image, text = device:name(), subText = device:uid() })
         end
         return choices
     end)
