401d701a1362dcc072285512c8a6552f0aa3206d
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2025-11-03 10:05:03 -0600

    refactor(sbar): Change to more dynamically load cisco apps to sbar, since app-name changed when upgrading to Tahoe

--- macos/sketchybar/items/network.lua
+++ macos/sketchybar/items/network.lua
@@ -1,1 +1,10 @@
-Sketchybar.add("alias", "Cisco Secure Client,Item-0", { position = "right" })
+---Sleep 5 to allow sketchybar to start, then add any cisco app icons as aliases
+---@param aliases table<string>
+---@param exit_code integer
+Sketchybar.exec("sleep 5 && sketchybar --query default_menu_items", function(aliases, exit_code)
+    for i, alias in ipairs(aliases) do
+        if alias:lower():match("cisco") then
+            Sketchybar.add("alias", alias, { position = "right" })
+        end
+    end
+end)
